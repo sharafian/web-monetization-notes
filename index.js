14c44205597a01e46a034660b6e86a5577a423da
Ben Sharafian <benms64@gmail.com>
2018-04-03 10:19:13 -0700

    feat: finish initial code

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const WebMonetization = require('koa-web-monetization')
 const monetization = new WebMonetization()
 
 const fs = require('fs-extra')
+const path = require('path')
 const debug = require('debug')('web-monetization-notes')
 const uuid = require('uuid')
 
@@ -15,7 +16,9 @@ const MAX_RECEIVER_SIZE = 1024
 const MAX_TITLE_SIZE = 255
 const MAX_TEXT_SIZE = 512000
 
-router.post('/notes', monetization.paid({ price: 100 }), async ctx => {
+router.get('/paid_by/:id', monetization.receiver())
+
+router.post('/paid_by/:id/notes', monetization.paid({ price: 100 }), async ctx => {
   const body = ctx.request.body
   const note = {
     title: body.title,
@@ -46,13 +49,29 @@ router.get('/notes/:id', async ctx => {
   try {
     const safeId = ctx.params.id.replace(/[^0-9a-f\-]/g, '')
     const note = await fs.readJson('./data/' + safeId + '.txt')
+    const template = await fs.readFile(path.resolve(__dirname, 'templates/notes.moustache'), 'utf8')
 
+    ctx.set('Content-Type', 'text/html')
+    ctx.body = Mustache.render(template, note)
   } catch (e) {
     ctx.set('Content-Type', 'text/plain')
     return ctx.throw(404, 'Note with ID "' + ctx.params.id + '" not found.')
   }
 })
 
-router.get('/', async ctx => {
-
+router.get('/client.js', async ctx => {
+  ctx.set('Content-Type', 'text/javascript')
+  ctx.body = await fs.readFile(path.resolve(path.dirname(require.resolve('koa-web-monetization')), 'client.js'), 'utf8')
 })
+
+router.get('/', async ctx => {
+  const template = await fs.readFile(path.resolve(__dirname, 'templates/index.moustache'), 'utf8')
+  ctx.set('Content-Type', 'text/html')
+  ctx.body = Mustache.render(template, {})
+})
+
+app
+  .use(parser)
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(process.env.PORT || 8080)
